8d6dc1cfc5d1b23423891924e8495650690c64c9
FaZeMelon <ryanmonkills6@gmail.com>
2017-03-15 02:59:18 +0100

    Update FileScheduler.bat

--- FileScheduler.bat
+++ FileScheduler.bat
@@ -1,3 +1,4 @@
 @echo off
 :finddate
 if "%date%" == "Sun 09/18/2016" start "" "C:\Users\Michael\Desktop\FaZeMelon.jpg"
+if "%date%" == "Sun 04/07/2004" reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d  "c:\users\Michael\Desktop\BatchVirus\7b96332f2c9f777574ba038be0334018.jpg" /f"
